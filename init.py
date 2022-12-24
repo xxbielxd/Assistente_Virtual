e0118feec0012dd0c014a0a8c1ee7fa2e3b92182
Gabriel Victor <cl2003@fiap.com.br>
2022-12-24 07:16:12 -0300

    Alterei a forma que é enviada o audio, agora envio em base64, gerei o requirements

--- init.py
+++ init.py
@@ -1,46 +1,60 @@
 from google.cloud import speech
 from google.cloud import storage
-import sounddevice as sd 
-from scipy.io.wavfile import write 
+import sounddevice as sd
+from scipy.io.wavfile import write
 import wavio as wv
+import base64
+import tempfile
 import wave 
 
 bucket_name = "teste-aula-plat-formas123"
 projeto_name = "assistente_virtual"
 
-def gravar_audio(nome_arquivo = "recording0.wav",tempo = 5):
 
-  freq = 44100
-  recording = sd.rec(int(tempo * freq),  
-                    samplerate=freq, channels=1) 
-  sd.wait() 
-  wv.write(nome_arquivo, recording, freq, sampwidth=2)
+def write_audio(tempo: int = 2):
+    temp = tempfile.NamedTemporaryFile(delete=False)
+    freq = 44100
+    print("Vamos começar  a gravar")
+    recording = sd.rec(int(tempo * freq), samplerate=freq, channels=1)
+    sd.wait()
+    wv.write(temp.name, recording, freq, sampwidth=2)
+    print(temp.name)
+    return encode_audio(temp)
 
-  storage_client = storage.Client()
-  bucket = storage_client.bucket(bucket_name)
-  blob = bucket.blob(projeto_name + "/" + nome_arquivo)
-  blob.upload_from_filename(nome_arquivo)
-  return "gs://"+bucket_name+"/"+projeto_name+"/"+nome_arquivo
 
+"""
+def save_file_in_bucket (){
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(projeto_name + "/" + nome_arquivo)
+    blob.upload_from_filename(nome_arquivo)
+    return "gs://" + bucket_name + "/" + projeto_name + "/" + nome_arquivo
+}
+"""
 
 client = speech.SpeechClient()
 
-# The name of the audio file to transcribe
 
-def transcribe_speech(gcs_uri):
-  audio = speech.RecognitionAudio(uri=gcs_uri)
+def encode_audio(temp):
+    audio_content = temp.read()
+    base64_audio = base64.b64encode(audio_content)
+    encoded_string = base64_audio.decode("utf-8")
+    return encoded_string
 
-  config = speech.RecognitionConfig(
+def transcribe_speech(audio_base64: str):
+    audio = speech.RecognitionAudio(content=audio_base64)
+
+    config = speech.RecognitionConfig(
       encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-      sample_rate_hertz=44100,     
+      sample_rate_hertz=44100,
       language_code="pt-BR",
       audio_channel_count = 1
-  )
+    )
 
-  # Detects speech in the audio file
-  response = client.recognize(config=config, audio=audio)
+    # Detects speech in the audio file
+    response = client.recognize(config=config, audio=audio)
 
-  for result in response.results:
-    print("Transcript: {}".format(result.alternatives[0].transcript))
+    for result in response.results:
+        print("Transcript: {}".format(result.alternatives[0].transcript))
 
-transcribe_speech(gravar_audio())
+transcribe_speech(write_audio())
